8d0e21727b5a2df3a4c23e3797049c6abedcc546
Ethan Cowan <ethan.cowan@gmail.com>
2020-08-22 14:12:53 -0600

    Implement proxy server for SOA front end

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
-
-import App from './App.jsx';
-
-ReactDom.render(<App />, document.getElementById('root'));
